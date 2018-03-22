f91288867e76a711ccf5cc60aac2cfe7c470c6f0
qiuhuasheng <huasheng.qiu@yuntu-inc.com>
2018-03-22 10:37:17 +0800

    add:  collection/finance

---
+++ module/collection/collection-dao/src/main/java/com/beitu/saas/collection/dao/SaasCollectionOrderDao.java
@@ -0,0 +1,14 @@
+package com.beitu.saas.collection.dao;
+
+import com.beitu.saas.collection.entity.SaasCollectionOrderEntity;
+import com.fqgj.common.base.BaseMapper;
+
+/**
+* User: fenqiguanjia
+* Date: 2018-03-21
+* Time: 20:58:19.360
+*/
+
+public interface SaasCollectionOrderDao  extends BaseMapper<SaasCollectionOrderEntity> {
+
+}
