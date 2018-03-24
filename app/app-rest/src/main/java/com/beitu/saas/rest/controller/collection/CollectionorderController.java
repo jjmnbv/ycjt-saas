828e79879b956166b9ce5847949a2b642980724c
qiuhuasheng <huasheng.qiu@yuntu-inc.com>
2018-03-24 15:02:31 +0800

    update: 催收列表选项2.0

---
+++ app/app-rest/src/main/java/com/beitu/saas/rest/controller/collection/CollectionorderController.java
@@ -0,0 +1,44 @@
+package com.beitu.saas.rest.controller.collection;
+
+import com.beitu.saas.app.common.RequestLocalInfo;
+import com.beitu.saas.channel.client.SaasCollectionOrderService;
+import com.beitu.saas.channel.param.CollectionOrderQueryParam;
+import com.beitu.saas.collection.vo.CollectionOrderInfoDetailVo;
+import com.beitu.saas.rest.controller.collection.response.CollectionOrderListResponse;
+import com.fqgj.common.api.Page;
+import com.fqgj.common.response.ModuleResponse;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WatsonQiu
+ * Date: 2018/3/24
+ * Time: 下午2:46
+ */
+@Api(description = "催收列表相关接口")
+@RestController
+@RequestMapping("/collection")
+public class CollectionOrderController {
+
+    @Autowired
+    private SaasCollectionOrderService saasCollectionOrderService;
+
+    /**
+     * 催收列表
+     *
+     * @param collectionOrderQueryParam
+     * @param page
+     * @return
+     */
+    @RequestMapping("/collectionOrderGet/list")
+    public ModuleResponse collectionDistributeGet(@RequestBody CollectionOrderQueryParam collectionOrderQueryParam, Page page) {
+        List<CollectionOrderInfoDetailVo> collectionOrderListByPage = saasCollectionOrderService.getCollectionOrderListByPage(collectionOrderQueryParam, page);
+        return new ModuleResponse<>(new CollectionOrderListResponse(collectionOrderListByPage), page);
+    }
+}
