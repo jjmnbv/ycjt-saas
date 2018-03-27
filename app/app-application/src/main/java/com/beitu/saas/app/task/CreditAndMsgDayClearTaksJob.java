260ee39a0fa4144f83ebcfc6a3e36c88cd6e288c
qiuhuasheng <huasheng.qiu@yuntu-inc.com>
2018-03-27 20:05:44 +0800

    update:  spring任务

---
+++ app/app-application/src/main/java/com/beitu/saas/app/task/CreditAndMsgDayClearTaksJob.java
@@ -0,0 +1,42 @@
+package com.beitu.saas.app.task;
+
+import com.beitu.saas.app.application.finance.SaasConsumeDayStatApplication;
+import com.beitu.saas.common.config.ConfigUtil;
+import com.fqgj.common.utils.StringUtils;
+import com.fqgj.log.factory.LogFactory;
+import com.fqgj.log.interfaces.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WatsonQiu
+ * Date: 2018/3/27
+ * Time: 下午7:56
+ */
+@Component
+@EnableScheduling
+public class CreditAndMsgDayClearTaksJob {
+    private static final Log LOGGER = LogFactory.getLog(CreditAndMsgDayClearTaksJob.class);
+
+    @Autowired
+    private ConfigUtil configUtil;
+    @Autowired
+    private SaasConsumeDayStatApplication saasConsumeDayStatApplication;
+
+    @Scheduled(cron = "0 30 1 * * ?")
+    public void creditAndMsgDayClear() {
+        LOGGER.info("== 点券和短信日清算任务开始 ==");
+        String taskSwitch = configUtil.getClearDayStatSwith();
+        if (StringUtils.isEmpty(taskSwitch) || taskSwitch.equals("false")) {
+            LOGGER.info("!====== 点券和短信每日清算任务未开启 ====!");
+            return;
+        }
+
+        saasConsumeDayStatApplication.creditAndMsgDayClear();
+        LOGGER.info("== 点券和短信日清算任务结束 ==");
+    }
+}
