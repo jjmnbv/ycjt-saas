da2334178ef730cd25b3f11018aaa3793eb816cf
qiuhuasheng <huasheng.qiu@yuntu-inc.com>
2018-03-27 21:17:21 +0800

    update:   催收列表

--- module/order/order-dao/src/main/java/com/beitu/saas/order/dao/impl/SaasOrderStatusHistoryDaoImpl.java
+++ module/order/order-dao/src/main/java/com/beitu/saas/order/dao/impl/SaasOrderStatusHistoryDaoImpl.java
@@ -4,6 +4,9 @@ import com.beitu.saas.order.dao.SaasOrderStatusHistoryDao;
 import com.beitu.saas.order.entity.SaasOrderStatusHistory;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
 * User: jungle
 * Date: 2018-03-25
@@ -13,4 +16,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class SaasOrderStatusHistoryDaoImpl extends AbstractBaseMapper<SaasOrderStatusHistory> implements SaasOrderStatusHistoryDao {
 
+    @Override
+    public SaasOrderStatusHistory selectOrderStatusHistoryByOrderNumb(String orderNumb) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("orderNumb", orderNumb);
+        return getSqlSession().selectOne(this.getStatement("selectOrderStatusHistoryByOrderNumb"), map);
+    }
 }
