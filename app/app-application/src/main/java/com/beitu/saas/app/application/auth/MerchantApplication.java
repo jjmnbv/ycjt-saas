2d2e09b27ca9de492fddf1ee57fa8402a080df4e
肖冲 <chong.xiao@beitu-inc.com>
2018-03-28 11:43:34 +0800

    add 添加机构,设置机构默认角色及赋权

--- app/app-application/src/main/java/com/beitu/saas/app/application/auth/MerchantApplication.java
+++ app/app-application/src/main/java/com/beitu/saas/app/application/auth/MerchantApplication.java
@@ -1,9 +1,9 @@
 package com.beitu.saas.app.application.auth;
 
-import com.beitu.saas.auth.entity.SaasAdmin;
-import com.beitu.saas.auth.entity.SaasMerchant;
-import com.beitu.saas.auth.service.SaasAdminService;
-import com.beitu.saas.auth.service.SaasMerchantService;
+import com.beitu.saas.auth.entity.*;
+import com.beitu.saas.auth.enums.ContractConfigTypeEnum;
+import com.beitu.saas.auth.enums.MerchantConfigTypeEnum;
+import com.beitu.saas.auth.service.*;
 import com.fqgj.common.utils.GenerOrderNoUtil;
 import com.fqgj.common.utils.MD5;
 import com.fqgj.common.utils.StringUtils;
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author xiaochong
@@ -30,8 +31,18 @@ public class MerchantApplication {
     @Autowired
     private RoleApplication roleApplication;
 
-    @Transactional
-    public void addMerchant(SaasMerchant saasMerchant,String password){
+    @Autowired
+    private SaasAdminRoleService saasAdminRoleService;
+
+    @Autowired
+    private SaasMerchantConfigService saasMerchantConfigService;
+
+    @Autowired
+    private SaasSmsConfigDictionaryService saasSmsConfigDictionaryService;
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public void addMerchant(SaasMerchant saasMerchant, String password) {
 
         //1.保存机构信息
         saasMerchant.setMerchantCode(GenerOrderNoUtil.generateOrderNo());
@@ -41,21 +52,57 @@ public class MerchantApplication {
         saasAdmin.setMerchantCode(saasMerchant.getMerchantCode());
         saasAdmin.setCode(GenerOrderNoUtil.generateOrderNo());
         saasAdmin.setJob("系统超级管理员");
-        saasAdmin.setName(StringUtils.isNotEmpty(saasMerchant.getCompanyName())?saasMerchant.getCompanyName():saasMerchant.getLenderName());
-        saasAdmin.setMobile(StringUtils.isNotEmpty(saasMerchant.getCompanyTel())?saasMerchant.getCompanyTel():saasMerchant.getLenderTel());
+        saasAdmin.setName(StringUtils.isNotEmpty(saasMerchant.getCompanyName()) ? saasMerchant.getCompanyName() : saasMerchant.getLenderName());
+        saasAdmin.setMobile(StringUtils.isNotEmpty(saasMerchant.getCompanyTel()) ? saasMerchant.getCompanyTel() : saasMerchant.getLenderTel());
         saasAdmin.setPassword(MD5.md5(password));
         saasAdmin.setCreateName("system");
         saasAdmin.setDefault(true);
         saasAdmin.setEnable(true);
         saasAdminService.create(saasAdmin);
         //3.添加机构默认角色
-
         //超级管理员
-        Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126);
-        Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126);
-        //roleApplication.addRoleAndEmpower("超级管理员","system",saasAdmin.getMerchantCode(),);
+        Long roleId = roleApplication.addRoleAndEmpower("超级管理员", "system", saasAdmin.getMerchantCode(),
+                Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126),
+                Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155)
+        );
+        //子管理员
+        roleApplication.addRoleAndEmpower("子管理员", "system", saasAdmin.getMerchantCode(),
+                Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126),
+                Arrays.asList(109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152)
+        );
+        //审核
+        roleApplication.addRoleAndEmpower("审核", "system", saasAdmin.getMerchantCode(),
+                Arrays.asList(113, 114, 115, 116, 117),
+                Arrays.asList(114, 115, 116, 117, 118, 119, 136, 137, 138, 139, 140, 141)
+        );
+        //财务
+        roleApplication.addRoleAndEmpower("财务", "system", saasAdmin.getMerchantCode(),
+                Arrays.asList(113, 118, 119, 120),
+                Arrays.asList(142, 143, 144, 145, 146, 147, 148, 149, 150)
+        );
+        //催收
+        roleApplication.addRoleAndEmpower("催收", "system", saasAdmin.getMerchantCode(),
+                Arrays.asList(121),
+                Arrays.asList(151, 152)
+        );
 
         //4.给用户赋权
+        SaasAdminRole saasAdminRole = new SaasAdminRole();
+        saasAdminRole.setAdminCode(saasAdmin.getCode());
+        saasAdminRole.setRoleId(roleId);
+        saasAdminRoleService.create(saasAdminRole);
         //5.添加默认机构配置
+        SaasMerchantConfig saasMerchantConfig = new SaasMerchantConfig();
+        saasMerchantConfig.setConfigEnum(ContractConfigTypeEnum.COMPANY_CONTRACT.getKey());
+        saasMerchantConfig.setConfigType(MerchantConfigTypeEnum.CONTRACT_CONFIG.getKey().longValue());
+        saasMerchantConfigService.create(saasMerchantConfig);
+
+        List<SaasSmsConfigDictionary> smsConfig = saasSmsConfigDictionaryService.getAllSmsConfig();
+        smsConfig.forEach(saasSmsConfigDictionary -> {
+            SaasMerchantConfig entity = new SaasMerchantConfig();
+            saasMerchantConfig.setConfigEnum(saasSmsConfigDictionary.getId().intValue());
+            saasMerchantConfig.setConfigType(MerchantConfigTypeEnum.SMS_CONFIG.getKey().longValue());
+            saasMerchantConfigService.create(entity);
+        });
     }
 }
