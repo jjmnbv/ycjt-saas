24c894437c80800c8851da414f4ccaefdbae1713
linanjun <nanjun.li@beitu-inc.com>
2018-03-29 18:37:41 +0800

    修改 首页信息

---
+++ app/app-rest/src/main/java/com/beitu/saas/rest/controller/h5/response/ChannelInfoResponse.java
@@ -0,0 +1,42 @@
+package com.beitu.saas.rest.controller.h5.response;
+
+import com.fqgj.common.api.ResponseData;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @author linanjun
+ * @create 2018/3/29 下午6:28
+ * @description
+ */
+@ApiModel(value = "渠道展示信息")
+public class ChannelInfoResponse implements ResponseData {
+
+    @ApiModelProperty(value = "头部提示")
+    private String headerTitle;
+
+    @ApiModelProperty(value = "图片提示")
+    private String picTitle;
+
+    public ChannelInfoResponse(String headerTitle, String picTitle) {
+        this.headerTitle = headerTitle;
+        this.picTitle = picTitle;
+    }
+
+    public String getHeaderTitle() {
+        return headerTitle;
+    }
+
+    public void setHeaderTitle(String headerTitle) {
+        this.headerTitle = headerTitle;
+    }
+
+    public String getPicTitle() {
+        return picTitle;
+    }
+
+    public void setPicTitle(String picTitle) {
+        this.picTitle = picTitle;
+    }
+
+}
