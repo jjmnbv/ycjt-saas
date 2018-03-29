e5a9ee2fcd93aec3d6531b99f0fb94e43a5efbb7
肖冲 <chong.xiao@beitu-inc.com>
2018-03-29 20:27:19 +0800

    fix h5文件不能上传

--- app/app-application/src/main/java/com/beitu/saas/app/application/auth/AdminInfoApplication.java
+++ app/app-application/src/main/java/com/beitu/saas/app/application/auth/AdminInfoApplication.java
@@ -4,6 +4,7 @@ import com.beitu.saas.auth.entity.SaasAdmin;
 import com.beitu.saas.auth.entity.SaasAdminRole;
 import com.beitu.saas.auth.entity.SaasAdminToken;
 import com.beitu.saas.auth.entity.SaasRolePermission;
+import com.beitu.saas.auth.enums.AdminErrorEnum;
 import com.beitu.saas.auth.service.SaasAdminRoleService;
 import com.beitu.saas.auth.service.SaasAdminService;
 import com.beitu.saas.auth.service.SaasAdminTokenService;
@@ -12,6 +13,7 @@ import com.fqgj.common.entity.BaseEntity;
 import com.fqgj.common.utils.CollectionUtils;
 import com.fqgj.common.utils.GenerOrderNoUtil;
 import com.fqgj.common.utils.TimeUtils;
+import com.fqgj.exception.common.ApplicationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -81,6 +83,9 @@ public class AdminInfoApplication {
 
     @Transactional(rollbackFor = Exception.class)
     public void addAdminAndRole(SaasAdmin saasAdmin,Long roleId){
+        if (saasAdminService.hasRegisteredMobile(saasAdmin.getMobile())){
+            throw new ApplicationException(AdminErrorEnum.MOBILE_EXIST);
+        }
         saasAdmin.setCode(GenerOrderNoUtil.generateOrderNo());
         SaasAdmin entity = (SaasAdmin) saasAdminService.create(saasAdmin);
         SaasAdminRole saasAdminRole = new SaasAdminRole();
