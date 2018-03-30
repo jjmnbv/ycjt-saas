38c3eee45e724f11f04bde23000286ba912dfc4b
linanjun <nanjun.li@beitu-inc.com>
2018-03-30 16:21:17 +0800

    application 设计

--- app/app-intergration/src/main/java/com/beitu/saas/intergration/esign/EsignIntegrationService.java
+++ app/app-intergration/src/main/java/com/beitu/saas/intergration/esign/EsignIntegrationService.java
@@ -16,12 +16,32 @@ import java.io.InputStream;
  */
 public interface EsignIntegrationService {
 
+    /**
+     * 新增 e签宝 个人账户
+     * @param personAccountParam
+     * @return
+     */
     AddPersonAccountSuccessDto addPersonAccount(PersonAccountParam personAccountParam);
 
+    /**
+     * 新增 e签宝 机构账户
+     * @param organizeAccountParam
+     * @return
+     */
     AddOrganizeAccountSuccessDto addOrganizeAccount(OrganizeAccountParam organizeAccountParam);
 
+    /**
+     * 签署 借款协议
+     * @param loanContractSignParam
+     * @return
+     */
     InputStream doLoanContractSign(LoanContractSignParam loanContractSignParam);
 
+    /**
+     * 签署 授权协议
+     * @param licenseContractSignParam
+     * @return
+     */
     InputStream doLicenseContractSign(LicenseContractSignParam licenseContractSignParam);
 
 }
