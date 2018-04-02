069bb7f5efa827e10517b758a83627b51dc4cadc
qiuhuasheng <huasheng.qiu@yuntu-inc.com>
2018-04-02 21:42:54 +0800

    update:  二维码生成

--- app/app-rest/src/main/java/com/beitu/saas/rest/controller/channel/SaasChannelController.java
+++ app/app-rest/src/main/java/com/beitu/saas/rest/controller/channel/SaasChannelController.java
@@ -7,19 +7,18 @@ import com.beitu.saas.app.application.channel.vo.ChannelStatDetailVo;
 import com.beitu.saas.app.common.RequestLocalInfo;
 import com.beitu.saas.auth.entity.SaasAdmin;
 import com.beitu.saas.channel.domain.SaasChannelDetailVo;
-import com.beitu.saas.channel.domain.SaasModuleVo;
 import com.beitu.saas.channel.param.ChannelStatQueryParam;
 import com.beitu.saas.channel.param.SaasChannelParam;
 import com.beitu.saas.channel.client.SaasChannelService;
 import com.beitu.saas.channel.domain.SaasChannelVo;
 import com.beitu.saas.channel.enums.ChannelErrorCodeEnum;
 import com.beitu.saas.channel.param.SaasChannelRiskSettingsParam;
-import com.beitu.saas.channel.vo.ChannelStatVo;
 import com.beitu.saas.rest.controller.channel.request.ChannelStatQueryRequestParam;
 import com.beitu.saas.rest.controller.channel.request.SaasChannelQueryRequestParam;
 import com.beitu.saas.rest.controller.channel.request.SaasChannelRequestParam;
 import com.beitu.saas.rest.controller.channel.request.SaasOperateChannelRequestParam;
 import com.beitu.saas.rest.controller.channel.response.*;
+import com.fqgj.common.api.ApiResponse;
 import com.fqgj.common.api.Page;
 import com.fqgj.common.api.Response;
 import com.fqgj.common.response.ModuleResponse;
@@ -27,12 +26,21 @@ import com.fqgj.common.utils.CollectionUtils;
 import com.fqgj.common.utils.StringUtils;
 import com.fqgj.log.factory.LogFactory;
 import com.fqgj.log.interfaces.Log;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -144,7 +152,40 @@ public class SaasChannelController {
     }
 
     /**
-     * 获取渠道列表
+     * 二维码请求
+     *
+     * @param resp
+     * @param qrUrl
+     * @throws IOException
+     */
+    @RequestMapping(value = "/getQRcode", method = {RequestMethod.POST, RequestMethod.GET})
+    @ApiOperation(value = "获取二维码", response = ApiResponse.class)
+    public ApiResponse getQRcode(HttpServletResponse resp, @RequestParam String qrUrl) throws IOException {
+        if (qrUrl != null && !"".equals(qrUrl)) {
+            resp.setHeader("Content-type", "image/png");
+            OutputStream stream = null;
+            try {
+                int width = 400;//图片的宽度
+                int height = 400;//高度
+                stream = resp.getOutputStream();
+
+                QRCodeWriter writer = new QRCodeWriter();
+                BitMatrix m = writer.encode(qrUrl, BarcodeFormat.QR_CODE, height, width);
+                MatrixToImageWriter.writeToStream(m, "png", stream);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            } finally {
+                if (stream != null) {
+                    stream.flush();
+                    stream.close();
+                }
+            }
+        }
+        return new ApiResponse();
+    }
+
+    /**
+     * 获取渠道统计列表
      *
      * @return
      */
