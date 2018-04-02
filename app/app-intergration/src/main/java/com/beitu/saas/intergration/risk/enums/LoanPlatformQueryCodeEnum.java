b2bbc859831a7a30e2321aa779cbbaea10d00415
Chengyu Lin <chengyu.lin@beitu-inc.com>
2018-04-02 13:51:57 +0800

    update

--- app/app-intergration/src/main/java/com/beitu/saas/intergration/risk/enums/LoanPlatformQueryCodeEnum.java
+++ app/app-intergration/src/main/java/com/beitu/saas/intergration/risk/enums/LoanPlatformQueryCodeEnum.java
@@ -4,7 +4,8 @@ public enum LoanPlatformQueryCodeEnum {
     
     SUCCESS(1000, "成功"),
     PARAM_ERROR(1001, "输入参数有误"),
-    RESPONSE_ERROR(1002, "接口响应有误");
+    RESPONSE_ERROR(1002, "接口响应有误"),
+    RESULT_ERROR(1003, "接口结果错误");
     
     private Integer code;
     
