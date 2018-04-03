cf5ef81388b91149a4f52269c9ea03c82852e501
Chengyu Lin <chengyu.lin@beitu-inc.com>
2018-04-03 20:42:02 +0800

    update

--- app/app-application/src/main/java/com/beitu/saas/app/application/credit/LoanPlatformApplication.java
+++ app/app-application/src/main/java/com/beitu/saas/app/application/credit/LoanPlatformApplication.java
@@ -142,7 +142,7 @@ public class LoanPlatformApplication {
         }
         redisClient.set(RedisKeyConsts.H5_LOAN_PLATFORM_CRAWLING, timestamp, TimeConsts.THREE_MINUTE, userCode, website);
         return "redirect:" + configUtil.getAddressURLPrefix() + configUtil.getH5AddressURLPrefix()
-                + "?channel=" + channelCode + "/formList";
+                + "?channel=" + channelCode + "#/formList";
     }
     
     private Boolean validateSign(JuxinliCallbackDataPojo pojo) {
