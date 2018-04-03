c2de7171ce6364941adfffa58fe7f74624b64b33
linanjun <nanjun.li@beitu-inc.com>
2018-04-03 10:41:22 +0800

    完善 合同签署 pdf

--- app/app-application/src/main/java/com/beitu/saas/app/application/contract/consts/ContractConsts.java
+++ app/app-application/src/main/java/com/beitu/saas/app/application/contract/consts/ContractConsts.java
@@ -5,12 +5,12 @@ package com.beitu.saas.app.application.contract.consts;
  * @create 2018/4/2 下午9:49
  * @description
  */
-public interface ContractConsts {
+public class ContractConsts {
 
-    String AUTHORIZATION_PDF_PATH = "";
+    String AUTHORIZATION_PDF_PATH = ContractConsts.class.getClassLoader().getResource("").getPath() + "authorization.pdf";
 
-    String LOAN_PDF_PATH = "";
+    String LOAN_PDF_PATH = ContractConsts.class.getClassLoader().getResource("").getPath() + "licence.pdf";
 
-    String EXTEND_PDF_PATH = "";
+    String EXTEND_PDF_PATH = ContractConsts.class.getClassLoader().getResource("").getPath() + "extendTerm.pdf";
 
 }
