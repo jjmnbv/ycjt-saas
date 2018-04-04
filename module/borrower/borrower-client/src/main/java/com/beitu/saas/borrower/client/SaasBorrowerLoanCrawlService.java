04df48d064207aabcbb55bb0b1501ad603b363f3
Chengyu Lin <chengyu.lin@beitu-inc.com>
2018-04-04 16:45:31 +0800

    update

---
+++ module/borrower/borrower-client/src/main/java/com/beitu/saas/borrower/client/SaasBorrowerLoanCrawlService.java
@@ -0,0 +1,16 @@
+package com.beitu.saas.borrower.client;
+
+import com.beitu.saas.borrower.domain.SaasBorrowerLoanCrawlVo;
+import com.fqgj.common.base.BaseService;
+import com.fqgj.common.entity.BaseEntity;
+
+/**
+ * User: linchengyu
+ * Date: 2018-04-04
+ * Time: 16:09:15.869
+ */
+public interface SaasBorrowerLoanCrawlService<T extends BaseEntity> extends BaseService<T> {
+    
+    Boolean addSaasBorrowerLoanCrawl(SaasBorrowerLoanCrawlVo saasBorrowerLoanCrawlVo);
+    
+}
