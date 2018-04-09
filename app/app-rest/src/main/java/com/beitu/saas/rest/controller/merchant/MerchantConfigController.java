ccea996aa50b7500540427979b6d291a2d89e48b
肖冲 <chong.xiao@beitu-inc.com>
2018-04-09 21:39:53 +0800

    update 流量一天设置一次

--- app/app-rest/src/main/java/com/beitu/saas/rest/controller/merchant/MerchantConfigController.java
+++ app/app-rest/src/main/java/com/beitu/saas/rest/controller/merchant/MerchantConfigController.java
@@ -2,13 +2,17 @@ package com.beitu.saas.rest.controller.merchant;
 
 import com.beitu.saas.app.common.RequestLocalInfo;
 import com.beitu.saas.auth.domain.MerchantContractInfoVo;
+import com.beitu.saas.common.consts.RedisKeyConsts;
+import com.beitu.saas.common.utils.DateUtil;
 import com.beitu.saas.merchant.client.SaasMerchantFlowConfigService;
 import com.beitu.saas.merchant.client.domain.SaasMerchantFlowConfigVo;
 import com.beitu.saas.merchant.client.enums.MerchantFlowNumEnum;
 import com.beitu.saas.rest.controller.merchant.request.SaveConfigRequest;
 import com.beitu.saas.rest.controller.merchant.response.FlowConfigInfoResponse;
+import com.fqgj.base.services.redis.RedisClient;
 import com.fqgj.common.api.Response;
 import com.fqgj.common.api.annotations.ParamsValidate;
+import com.fqgj.common.api.exception.ApiErrorException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
@@ -31,6 +35,9 @@ public class MerchantConfigController {
     @Autowired
     private SaasMerchantFlowConfigService saasMerchantFlowConfigService;
 
+    @Autowired
+    private RedisClient redisClient;
+
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     @ApiOperation(value = "获取机构推荐流量配置", response = FlowConfigInfoResponse.class)
     public Response configInfo() {
@@ -42,6 +49,7 @@ public class MerchantConfigController {
         }
         return Response.ok().putData(flowConfigInfoResponse);
     }
+
     @RequestMapping(value = "/flow-num/map", method = RequestMethod.GET)
     @ApiOperation(value = "每日最大推送量枚举")
     public Response flowNum() {
@@ -52,8 +60,13 @@ public class MerchantConfigController {
     @ParamsValidate
     @ApiOperation(value = "保存流量设置")
     public Response saveConfig(@RequestBody SaveConfigRequest request) {
-        //TODO 一天只能保存一次
         String merchantCode = RequestLocalInfo.getCurrentAdmin().getSaasAdmin().getMerchantCode();
+        Object o = redisClient.get(RedisKeyConsts.SAAS_MERCHANT_FLOW_CONFIG, merchantCode);
+        if (null != o) {
+            throw new ApiErrorException("流量一天只能设置一次");
+        }
+        redisClient.set(RedisKeyConsts.SAAS_MERCHANT_FLOW_CONFIG, merchantCode, merchantCode);
+        redisClient.expireAt(RedisKeyConsts.SAAS_MERCHANT_FLOW_CONFIG, DateUtil.getNextDayBeginTime(), merchantCode);
         SaasMerchantFlowConfigVo vo = new SaasMerchantFlowConfigVo();
         BeanUtils.copyProperties(request, vo);
         vo.setMerchantCode(merchantCode);
